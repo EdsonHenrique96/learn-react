8d7c908770cf43d228b88adf5ef0322520168d83
Edson Henrique <henrique.ti1196@gmail.com>
2018-06-08 20:21:28 -0300

    learn react

--- ex/field.jsx
+++ ex/field.jsx
@@ -7,11 +7,21 @@ class Field extends Component {
         this.handleChange = this.handleChange.bind(this);
     }
 
+    componentWillMount() {
+        console.log('=== componentWillMount 1');
+    }
+
+    componentDidMount() {
+        console.log('=== componentDidMount 3');
+        // this.setState({ value: 'componentDidMount' });
+    }
+
     handleChange(event) {
         this.setState({ value: event.target.value });
     }
 
     render() {
+        console.log('=== render 2');
         return (
             <div>
                 <label>{this.state.value}</label><br/>
